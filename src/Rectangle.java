b8a846f78bdacc998fc3e470db1ca86a7ed4769a
Max de Visser <m.madv96@gmail.com>
2021-03-12 14:25:16 +0100

    Vi har optimeret koden og ryttet op
    Der er lavet en metode til at regne distancen mellem to punkter. Yderligere har vi lavet computeCenter for trekanten.
    Se "Logbog 12.3.21" for mere info.
    
    Co-Authored-By: sebastianrohr <73996470+sebastianrohr@users.noreply.github.com>
    Co-Authored-By: SimonHindsgaul <79086677+simonhindsgaul@users.noreply.github.com>

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -3,7 +3,7 @@ public class Rectangle extends Shape implements ShapeMethods{
     double length;
     double height;
 
-    public Rectangle(int posX, int posY, double l, double h) { //constructor. Oprettes med koordinatorne svarende til venstre øvre hjørne
+    public Rectangle(double posX, double posY, double l, double h) { //constructor. Oprettes med koordinatorne svarende til venstre øvre hjørne
         super(posX, posY);
         this.length = l;
         this.height = h;
@@ -21,20 +21,18 @@ public class Rectangle extends Shape implements ShapeMethods{
         return rectangleCircumference;
     }
 
-    //Kender du ik johnson??????
-
     @Override
     public Point computeCenter() {
-        Point rectangleCenter = new Point(xCoordinate+(length/2), yCoordinate+(height/2));
+        Point rectangleCenter = new Point(drawPointX +(length/2), drawPointY +(height/2));
         return rectangleCenter;
     }
 
     @Override
     public boolean checkPoint(double pointX, double pointY) {
-        double distanceX = Math.sqrt(pointX- xCoordinate)*(pointX - xCoordinate); //udregner afstanden fra x-koordinat til x-koordinat
-        double distanceY = Math.sqrt(pointY- yCoordinate)*(pointY- yCoordinate);
+        double distanceX = Math.sqrt(pointX - drawPointX)*(pointX - drawPointX); //udregner afstanden fra x-koordinat til x-koordinat
+        double distanceY = Math.sqrt(pointY - drawPointY)*(pointY- drawPointY);
 
-        if(pointX > xCoordinate && xCoordinate + length > pointX && pointY > yCoordinate && yCoordinate-length < pointY){
+        if(pointX > drawPointX && drawPointX + length > pointX && pointY > drawPointY && drawPointY -length < pointY){
             return true;
         }
         return false;
--- src/Triangle.java
+++ src/Triangle.java
@@ -1,39 +1,40 @@
 public class Triangle extends Shape implements ShapeMethods {
+    //vi antager at alle trekanter er retvinklede
+    double X2, Y2, X3, Y3; //triangles are represented as three points, and always have one 90 degree angle
 
-    double X2, Y2, X3, Y3; //triangles are represented as three points
-
-    public Triangle(int posX, int posY, int posX2, int posY2, int posX3, int posY3) {
+    public Triangle(double posX, double posY, double posX2, double posY2) {
         super(posX, posY);
         this.X2 = posX2;
         this.Y2 = posY2;
-        this.X3 = posX3;
-        this.Y3 = posY3;
+        this.X3 = posX;
+        this.Y3 = posY2;
     }
 
     @Override
     public double computeAreal() {
-        double distance1 = Math.sqrt((X2 - xCoordinate) * (X2 - xCoordinate) + (Y2 - yCoordinate) * (Y2 - yCoordinate));
-        double distance2 = Math.sqrt((X2 - X3) * (X2 - X3) + (Y2 - Y3) * (Y2 - Y3));
+        double distance1 = distanceBetweenTwoPoints(drawPointX, drawPointY,X3,Y3);
+        double distance2 = distanceBetweenTwoPoints(X2,Y2,X3,Y3);
         double triangleAreal = (distance1 * distance2)/2; // dobbelttjek at den her udregning er korrekt haha
         return triangleAreal;
     }
 
     @Override
     public double computeCircumference() {
-        double distance1 = Math.sqrt((X2 - xCoordinate) * (X2 - xCoordinate) + (Y2 - yCoordinate) * (Y2 - yCoordinate));
-        double distance2 = Math.sqrt((X2 - X3) * (X2 - X3) + (Y2 - Y3) * (Y2 - Y3));
-        double distance3 = Math.sqrt((X3-xCoordinate) * (X3-xCoordinate) + (Y3 - yCoordinate) * (Y2 - yCoordinate));
+        double distance1 = distanceBetweenTwoPoints(drawPointX, drawPointY,X2,Y2);
+        double distance2 = distanceBetweenTwoPoints(X2,Y2,X3,Y3);
+        double distance3 = distanceBetweenTwoPoints(drawPointX, drawPointY, X3,Y3);
         return distance1 + distance2 + distance3;
     }
 
     @Override
     public Point computeCenter() {
-        return null;
+        Point triangleCenter = new Point((drawPointX+X2+X3)/3, (drawPointY +Y2+Y3)/3);
+        return triangleCenter;
     }
 
     @Override
     public boolean checkPoint(double x, double y) {
+        
         return false;
     }
-
 }
