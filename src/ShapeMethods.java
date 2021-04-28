5db63b626a3d2b76889706d506c98e947dca309c
Max de Visser <m.madv96@gmail.com>
2021-04-28 13:31:11 +0200

    Delete src directory

--- src/ShapeMethods.java
+++
@@ -1,6 +0,0 @@
-public interface ShapeMethods {
-    public double computeAreal();
-    public double computeCircumference();
-    public void computeCenter();
-    public boolean checkPoint(double x, double y);
-}
